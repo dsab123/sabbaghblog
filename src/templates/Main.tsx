adfbd4db1bb21f598dbb9927cfb432a9d3b33534
dsab123 <dsabbaghumd@gmail.com>
2022-12-21 09:29:08 -0500

    add blagh link

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -27,7 +27,7 @@ const Main = (props: IMainProps) => (
           <Navbar>
             <li className="mr-6">
               <Link href="/">
-                <a>Home</a>
+                <a>Home (Blagh)</a>
               </Link>
             </li>
             <li className="mr-6">
