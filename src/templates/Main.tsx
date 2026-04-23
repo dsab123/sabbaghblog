aa1db4f38ca0c61034f434f49e679e2e3c469652
Daniel Sabbagh <dsabbaghumd@gmail.com>
2026-04-22 20:46:06 -0400

    remove creativedesignsguru mark, i've done a lot since I started this blog

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -81,13 +81,6 @@ const Main = (props: IMainProps) => {
           <span role="img" aria-label="Love">
             ♥
           </span>{' '}
-          by <a href="https://creativedesignsguru.com">CreativeDesignsGuru</a>
-          {/*
-           * PLEASE READ THIS SECTION
-           * We'll really appreciate if you could have a link to our website
-           * The link doesn't need to appear on every pages, one link on one page is enough.
-           * Thank you for your support it'll mean a lot for us.
-           */}
         </div>
       </div>
     </div>
